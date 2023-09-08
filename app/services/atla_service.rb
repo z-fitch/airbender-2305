48482490bd54a93cdd5e14700e50a6ae6acae4c6
Zanna Fitch <zannafitch2004@gmail.com>
2023-09-08 10:28:36 -0500

    Feat: add API connection

---
+++ app/services/atla_service.rb
@@ -0,0 +1,15 @@
+class ParksService
+
+  def nation_search(nation)
+    get_url("api/v1/characters?affiliation=#{nation}")
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: "https://last-airbender-api.fly.dev/")
+  end
+end
